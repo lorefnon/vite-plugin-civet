cc25a74839477f16f5e9bfd42dee3728a15b7415
Erik Demaine <edemaine@mit.edu>
2023-08-08 11:44:13 -0400

    WIP transform rewrite (very messy)

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { load as loadHTML } from 'cheerio'
 import type { FilterPattern } from '@rollup/pluginutils'
 import { createFilter } from '@rollup/pluginutils'
 import civet from '@danielx/civet'
+import fs from 'fs/promises'
 
 interface PluginOptions {
   outputTransformerPlugin?: string | string[] | {
@@ -19,6 +20,8 @@ interface PluginOptions {
     options?: { ssr?: boolean }
   ) => TransformResult | Promise<TransformResult>
 }
+
+const TRANSFORM_SUFFIX = '?civet'
 
 export default function plugin(pluginOpts: PluginOptions = {}): Plugin {
   const filter = createFilter(
@@ -85,29 +88,95 @@ export default function plugin(pluginOpts: PluginOptions = {}): Plugin {
     },
 
     async resolveId(id, importer, options) {
-      if (id.match(/\.civet/)) {
-        const [pathPart, queryPart] = id.split('?')
-        if (pathPart.match(/\.civet\.js$/) && queryPart === 'transform') {
-          const transformedId = pathPart.replace(/\.js$/, '')
-          const resolution = await this.resolve(
-            transformedId,
-            importer,
-            options,
-          )
-          return resolution?.id
-        }
-      }
-    },
-
-    async transform(code, id, options) {
       if (!filter(id))
         return null
+      if (id.startsWith('C:/')) return null
+      console.log('resolveId', id, options)
+      const resolution = await this.resolve(id, importer, {
+        skipSelf: true,
+        ...options,
+        assertions: {civet: "true"},
+      })
+      console.log('resolved', resolution)
+      if (resolution && !resolution.external) {
+        /*
+        console.log('calling load')
+        const moduleInfo = await this.load({...resolution,
+          assertions: {civet: "true"},
+          meta: {civet: {loading: true}}})
+        console.log(moduleInfo)
+        */
+        const transformedId = `${id}.${outputExtension}${TRANSFORM_SUFFIX}`
+        console.log('resolveId', id, '->', transformedId)
+        return `${resolution.id}.${outputExtension}${TRANSFORM_SUFFIX}`
+      }
+      return resolution
+      /*
+      const transformedId = `${id}.${outputExtension}?transform`
+      const resolution = await this.resolve(
+        transformedId,
+        importer,
+        options,
+      )
+      console.log('resolved to', resolution)
+      return resolution?.id
+      */
+    },
+
+    async load(id) {
+      if (!id.endsWith(TRANSFORM_SUFFIX))
+        return null
+      // Strip off `.${outputExtension}${TRANSFORM_SUFFIX}` from resolveId
+      const filename = id.slice(0,
+        -(1 + outputExtension.length + TRANSFORM_SUFFIX.length))
+      const code = await fs.readFile(filename, 'utf-8')
+      return {code, meta: {civet: {loading: true}}}
+      /*
       let transformed: TransformResult = {
         code: civet.compile(code, {
           inlineMap: true,
-          filename: id,
+          filename,
           js: stripTypes,
         } as any) as string,
+        map: null,
+        //id: id.slice(0, -TRANSFORM_SUFFIX.length),
+      }
+      */
+      /*
+      if (pluginOpts.transformOutput)
+        transformed = await pluginOpts.transformOutput(transformed.code, id)
+      for (const parentPlugin of parentPlugins) {
+        if (parentPlugin?.transform) {
+          const parentTransformHook = parentPlugin.transform
+          const transformFn = (typeof parentTransformHook === 'function')
+            ? parentTransformHook
+            : parentTransformHook.handler
+          const transformResult = await transformFn.apply(this, [
+            transformed.code,
+            `${id}.${outputExtension}`,
+          ])
+          if (typeof transformResult === 'string')
+            transformed.code = transformResult
+          else if (transformResult != null)
+            Object.assign(transformed, transformResult)
+        }
+      }
+      */
+      console.log('load transformed', transformed)
+      return transformed
+    },
+
+    async transform(code, id, options) {
+      if (!id.endsWith(TRANSFORM_SUFFIX))
+        return null
+      console.log('transform', id)
+      const jsId = id.slice(0, -TRANSFORM_SUFFIX.length)
+      let transformed: TransformResult = {
+        code: code, /*civet.compile(code, {
+          inlineMap: true,
+          filename: id,
+          js: stripTypes,
+        } as any) as string,*/
         map: null,
       }
       if (pluginOpts.transformOutput)
@@ -121,7 +190,7 @@ export default function plugin(pluginOpts: PluginOptions = {}): Plugin {
             : parentTransformHook.handler
           const transformResult = await transformFn.apply(this, [
             transformed.code,
-            `${id}.${outputExtension}`,
+            jsId, //`${id}.${outputExtension}`,
             options,
           ])
           if (typeof transformResult === 'string')
@@ -131,6 +200,7 @@ export default function plugin(pluginOpts: PluginOptions = {}): Plugin {
         }
       }
 
+      console.log('transformed', transformed)
       return transformed
     },
   }
